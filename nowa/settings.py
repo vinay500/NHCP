1d7051e324d58935df4a14cd66136bbc0647ba2b
vinay500 <vinaymadugula20@gmail.com>
2024-06-28 16:12:13 +0530

    added static files folder in staticfiles_dir

--- nowa/settings.py
+++ nowa/settings.py
@@ -147,7 +147,7 @@ HTML_MINIFY = False
 
 STATIC_URL = 'static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
