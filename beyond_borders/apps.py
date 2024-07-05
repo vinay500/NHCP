71082248bd720c820231765acf7f2dff712f854f
vinay500 <vinaymadugula20@gmail.com>
2024-07-05 16:06:28 +0530

    changed src of home care services imgs and moved imgs from icons folder to nhcp_imgs folder

--- beyond_borders/apps.py
+++ beyond_borders/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
-import logging
 
 class BeyondBordersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
--- home_care_services/apps.py
+++ home_care_services/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
-import logging
 
 
 class HomeCareServicesConfig(AppConfig):
